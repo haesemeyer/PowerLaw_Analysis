2889b203b44fa53674e9b371231d73e3932c23c3
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-11-14 16:01:58 -0500

    Added computation of bout radius and angle as well as assign each bout to a category

--- analysis_IHB.py
+++ analysis_IHB.py
@@ -67,3 +67,39 @@ if __name__ == '__main__':
             ax_s.set_ylabel('Instant speed [mm/s]')
             ax_s.set_xlabel('Time [s]')
             fig.tight_layout()
+
+        # for each bout compute the distance between start and endpoint as well as the heading change
+        bstarts = bouts[:, 0].astype(int)
+        bends = bouts[:, 2].astype(int)
+        boutRadius = np.sqrt((x_c[bends] - x_c[bstarts])**2 + (y_c[bends] - y_c[bstarts])**2)
+        boutTheta = np.abs(core.AssignDeltaAnglesToBouts(bouts, heading)[0])
+        # for each bout assign a category: -1 not in middle, 0 regular, 1 hunting, 2 escape
+        boutCategory = np.zeros(bouts.shape[0])
+        escape_frames = np.nonzero(escape)[0]
+        for i, (bs, be) in enumerate(zip(bstarts, bends)):
+            if not inmiddle[bs]:
+                boutCategory[i] = -1
+            else:
+                # NOTE: Escapes are identified by a single 1 but this does not align with our bout calls
+                if np.min(np.abs(bs - escape_frames)) <= 25:
+                    boutCategory[i] = 2
+                elif hunting[bs:be].sum() > 0:
+                    boutCategory[i] = 1
+                else:
+                    boutCategory[i] = 0
+
+        # plot bout radius vs. bout theta for our categories
+        with sns.axes_style('white'):
+            fig, ax = pl.subplots()
+            ax.scatter(boutRadius[boutCategory == 0]*ihb_pixelscale, boutTheta[boutCategory == 0], c='b', s=10,
+                       alpha=0.7, label='Regular')
+            ax.scatter(boutRadius[boutCategory == 1]*ihb_pixelscale, boutTheta[boutCategory == 1], c='g', s=10,
+                       alpha=0.7, label='Hunt')
+            ax.scatter(boutRadius[boutCategory == 2]*ihb_pixelscale, boutTheta[boutCategory == 2], c='r', s=10,
+                       alpha=0.7, label='Escape')
+            ax.legend()
+            ax.set_xlim(0)
+            ax.set_ylim(0, 180)
+            sns.despine(fig, ax)
+            ax.set_xlabel('Bout displacement [mm]')
+            ax.set_ylabel('Bout delta-angle [degrees]')
