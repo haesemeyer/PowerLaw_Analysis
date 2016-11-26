df02e9396afb4952d95959cb69ee46de29bf78a9
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-11-26 17:58:57 -0500

    Don’t allow selecting multiple files for wn analysis

--- analysis_wn.py
+++ analysis_wn.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     while sv != 'y' and sv != 'n':
         sv = input('Save figures? [y/n]')
     sv = sv == 'y'
-    fname = core.UiGetFile(diagTitle='Load data files')
+    fname = core.UiGetFile(diagTitle='Load data files', multiple=False)
     dfile = h5py.File(fname[0], 'r')
 
     all_fits = []  # list of log/log fits for each experiment
